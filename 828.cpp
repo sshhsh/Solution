6fd7e183c9d5bba9c4b6e71caefc535c74c41226
sshhsh <sshhsh@foxmail.com>
2018-07-23 20:38:50 +0800

    828 finished

--- 828.cpp
+++ 828.cpp
@@ -6,35 +6,23 @@
 
 using namespace std;
 
-int unique(string& s, int len){
-    int count[26]={0};
-    int tmp = 0;
+int uniqueLetterString(string S) {
+    int left[26];
+    int middle[26];
     int sum = 0;
-    for(int i=0;i<s.length();++i){
-        if(count[s[i]-'A'] == 1)
-            --tmp;
-        ++count[s[i]-'A'];
-        if(count[s[i]-'A'] == 1)
-            ++tmp;
-        if(i == len - 1)
-            sum += tmp;
-        if(i>=len){
-            if(count[s[i-len]-'A'] == 1)
-                --tmp;
-            --count[s[i-len]-'A'];
-            if(count[s[i-len]-'A'] == 1)
-                ++tmp;
-            sum += tmp;
-        }
+    for (int &i : left)
+        i = -1;
+    for (int &i : middle)
+        i = -1;
+    for(int i = 0; i<S.length(); ++i){
+        sum += (i-middle[S[i]-'A'])*(middle[S[i]-'A']-left[S[i]-'A']);
+        left[S[i]-'A'] = middle[S[i]-'A'];
+        middle[S[i]-'A'] = i;
     }
 
-    return sum%1000000007;
-}
-
-int uniqueLetterString(string S) {
-    int sum = 0;
-    for(int i = 1; i <= S.length(); ++i)
-        sum+=unique(S, i);
+    for(int i =0;i<26;++i){
+        sum += (S.length()-middle[i])*(middle[i]-left[i]);
+    }
     return sum%1000000007;
 }
 
